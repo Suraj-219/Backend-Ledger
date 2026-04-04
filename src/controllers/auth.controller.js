4e73bb482064eac2d7e82e766de72d25b88ad230
Suraj Pal <surajpal7442@gmail.com>
2026-04-04 13:44:30 +0530

    Fix bug

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,37 +8,44 @@ const jwt = require("jsonwebtoken")
 */
 
 async function userRegisterController(req, res) {
+    try {
+        const { email, password, name } = req.body
 
-    const { email, password, name } = req.body
+        const isExists = await userModel.findOne({ email })
 
-    const isExists = await userModel.findOne({
-        email: email
-    })
+        if (isExists) {
+            return res.status(422).json({
+                message: "User already exists with email.",
+                status: "failed"
+            })
+        }
 
-    if (isExists) {
-        return res.status(422).json({
-            message: "User already exists with email.",
-            status: "failed"
+        const user = await userModel.create({ email, password, name })
+
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "3d" }
+        )
+
+        res.cookie("token", token)
+
+        res.status(201).json({
+            user: {
+                _id: user._id,
+                email: user.email,
+                name: user.name
+            },
+            token
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: "Server error",
+            error: error.message
         })
     }
-
-    const user = await userModel.create({
-        email, password, name
-    })
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: "3d"})
-
-    res.cookies("token", token)
-
-    res.status(201).json({
-        user: {
-            _id: user._id,
-            email: user.email,
-            name: user.name
-        },
-        token
-    })
-
 }
 
 module.exports = {
