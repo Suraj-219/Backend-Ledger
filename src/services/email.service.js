2fe02af97f28b30f0672474661eadbe22fc38d28
Suraj Pal <surajpal7442@gmail.com>
2026-04-09 15:20:51 +0530

    Add email service for sending registration emails upon user registration

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -40,4 +40,15 @@ const sendEmail = async (to, subject, text, html) => {
   }
 };
 
-module.exports = sendEmail;
+
+async function sendRegistrationEmail(userEmail, name) {
+  const subject = 'Welcome to Backend Ledger!';
+  const text = `Hello ${name},\n\nThank you for registering with Backend Ledger. We're excited to have you on board!\n\nBest regards,\nThe Backend Ledger Team`;
+  const html = `<p>Hello ${name},</p><p>Thank you for registering at Backend Ledger. We're excited to have you on board!</p><p>Best regards,<br>The Backend Ledger Team</p>`;
+
+  await sendEmail(userEmail, subject, text, html);
+}
+
+module.exports = {
+  sendRegistrationEmail
+};
