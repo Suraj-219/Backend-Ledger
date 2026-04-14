d5d8db310a00946938817ae44d566233573e43c7
Suraj Pal <surajpal7442@gmail.com>
2026-04-14 14:23:16 +0530

    Validate request

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -21,11 +21,28 @@ const emailService = require("../services/email.service")
 
 async function createTransaction(req, res) {
 
+    /**
+     * 1. Validate request
+     */
     const { fromAccount, toAccount, amount, idempotencyKey } = req.body
 
     if (!fromAccount || !toAccount|| !amount || !idempotencyKey) {
         return res.status(400).json({
             message: "FromAccount, toAccount, amount and idempotencyKey are required"
+        })
+    }
+
+    const fromUserAccount = await accountModel.findOne({
+        _id: fromAccount,
+    })
+
+    const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+    })
+
+    if(!fromUserAccount || !toUserAccount) {
+        return res.status(400).json({
+            message: "Invalid fromAccount or toAccount"
         })
     }
 
