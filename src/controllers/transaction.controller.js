5746c54aba591cc701fc1ecc53e566e242a1bb07
Suraj Pal <surajpal7442@gmail.com>
2026-04-15 23:49:21 +0530

    Add idempotency key validation for transaction requests

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -46,4 +46,38 @@ async function createTransaction(req, res) {
         })
     }
 
+    /**
+     * 2. Validate idempotency Key
+     */
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey: idempotencyKey
+    })
+
+    if (isTransactionAlreadyExists) {
+        if(isTransactionAlreadyExists.status === "COMPLETED") {
+            res.status(200).json({
+                message: "Transaction already processed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "PENDING") {
+            res.status(200).json({
+                message: "Transaction is still processing"
+            })
+        }
+
+        if(isTransactionAlreadyExists.status === "FAILED") {
+            res.status(500).json({
+                message: "Transaction processing failed previously, please retry"
+            })
+        }
+
+        if (isTransactionAlreadyExists.status === "REVERSED") {
+            res.status(500).json({
+                message: "Transaction was reversed, please retry"
+            })
+        }
+    }
+
 }
