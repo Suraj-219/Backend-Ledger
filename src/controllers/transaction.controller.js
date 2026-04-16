bd0d0c460922d63fb1b898bc03a88c63f0f5b303
Suraj Pal <surajpal7442@gmail.com>
2026-04-16 12:17:47 +0530

    Check account status

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -55,29 +55,38 @@ async function createTransaction(req, res) {
 
     if (isTransactionAlreadyExists) {
         if(isTransactionAlreadyExists.status === "COMPLETED") {
-            res.status(200).json({
+            return res.status(200).json({
                 message: "Transaction already processed",
                 transaction: isTransactionAlreadyExists
             })
         }
 
         if (isTransactionAlreadyExists.status === "PENDING") {
-            res.status(200).json({
+            return res.status(200).json({
                 message: "Transaction is still processing"
             })
         }
 
         if(isTransactionAlreadyExists.status === "FAILED") {
-            res.status(500).json({
+            return res.status(500).json({
                 message: "Transaction processing failed previously, please retry"
             })
         }
 
         if (isTransactionAlreadyExists.status === "REVERSED") {
-            res.status(500).json({
+            return res.status(500).json({
                 message: "Transaction was reversed, please retry"
             })
         }
+    }
+
+    /**
+     * 3. Check account status
+     */
+    if (fromUserAccount.status !== "ACTIVE" || toUserAccount.status !== "ACTIVE") {
+        return res.status(400).json({
+            message: "Both fromAcoount and toAccount must be ACTIVE to process transaction"
+        })
     }
 
 }
