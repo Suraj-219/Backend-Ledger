4655717ae1e708831b615240ce2e3db233a372ff
Suraj Pal <surajpal7442@gmail.com>
2026-04-18 13:57:57 +0530

    Implement transaction completion and ledger entry creation

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -115,4 +115,24 @@ async function createTransaction(req, res) {
         status: "PENDING"
     }, { session })
 
+    const debitLedgerEntry = await ledgerModel.create({
+        account: fromAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "DEBIT"
+    }, { session })
+
+    const creditLedgerEntry = await ledgerModel.create({
+        account: toAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "CREDIT"
+    }, { session })
+
+    transaction.status = "COMPLETED"
+    await transaction.save({ session })
+
+    await session.commitTransaction()
+    session.endSession()
+
 }
